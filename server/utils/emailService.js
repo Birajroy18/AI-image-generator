c94cfaacc86a156dff1922a829fbd4ae8d69ad10
Biraj Roy <your-email@example.com>
2026-03-11 00:20:16 +0530

    Fix nodemailer method name from createTransporter to createTransport

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 // Create transporter (configure with your email service)
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
